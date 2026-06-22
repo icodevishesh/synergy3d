eb1807b8fb4653df7831f0da0e374bc8dd3f5c92
Vishesh Purkait <visheshpurkait23@gmail.com>
2026-06-22 10:22:02 +0530

    fix: add new script tag for whatsapp chat

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -38,12 +38,6 @@ export default function RootLayout({
         <LayoutWrapper>{children}</LayoutWrapper>
         <RevealObserver />
         <ModalsContainer />
-        {/* <Script
-            type="text/javascript"
-            src="https://d3mkw6s8thqya7.cloudfront.net/integration-plugin.js"
-            id="aisensy-wa-widget"
-            widget-id="aab24h"
-        /> */}
         <script 
           type="text/javascript"
           src="https://d3mkw6s8thqya7.cloudfront.net/integration-plugin.js"
